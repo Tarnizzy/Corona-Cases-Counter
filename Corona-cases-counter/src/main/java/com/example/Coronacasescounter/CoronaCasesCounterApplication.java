b7707d7ad9338cad31164e72c4c068ef48a5fd18
Hudson-obai <Hudsonobai@gmail.com>
2021-08-25 21:35:46 +0200

    Barebones of the project

---
+++ Corona-cases-counter/src/main/java/com/example/Coronacasescounter/CoronaCasesCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.Coronacasescounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaCasesCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaCasesCounterApplication.class, args);
+	}
+
+}
---
+++ Corona-cases-counter/src/test/java/com/example/Coronacasescounter/CoronaCasesCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Coronacasescounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaCasesCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
